d77f347cd5e69ab65d9d9fd365fd92a312149b65
reza <rahimi.r.g@gmail.com>
2016-10-17 01:30:54 -0400

    bugs solved in resource monitor

--- resource_monitor/src/node_monitor/publisher_node.py
+++ resource_monitor/src/node_monitor/publisher_node.py
@@ -15,21 +15,32 @@ ON_POSIX = 'posix' in sys.builtin_module_names
 def enqueue_output(out, queue):
     for line in iter(out.readline, b''):
         queue.put(line)
+    
     out.close()
-    
-def data_collector(process_name, write_to_file, sample_interval, net_interface, machine_name):
-    
-    if write_to_file:
-        home = expanduser("~")
-        log_file=open(home+'/chatter.log','w')
-    pid_list = psutil.pids();
+
+def reinitialize_process_list(process_name):
     my_process = [];
+    pid_list = psutil.pids();
     for p in pid_list:
         try:
             if psutil.Process(p).name() in  process_name:
                 my_process.append(psutil.Process(p))
         except psutil.NoSuchProcess:
-            pass        
+            pass
+    return my_process
+def data_collector(process_name, write_to_file, sample_interval, net_interface, machine_name):
+    process_name=process_name.split(',')
+    print process_name
+    if write_to_file:
+        home = expanduser("~")
+        log_file=open(home+'/chatter.log','w')
+    my_process=reinitialize_process_list(process_name)
+    while(len(my_process)<len(process_name)):
+       print "%d Process counts are not equal. Try again later %d"%(len(my_process),len(process_name))
+       rospy.sleep(1)
+       reinitialize_process_list(process_name)
+    
+    print "ALL PROCESSES ARE FOUND"   
             
             
     if not len(my_process):
@@ -53,8 +64,9 @@ def data_collector(process_name, write_to_file, sample_interval, net_interface, 
             for i in range(queue_size):
                 line = q.get_nowait();
                 bw_lines.append(line.strip())
-        
+                    
             for m in my_process:        
+                #print "**%d**%d**%d"%(m.name(),m.pid,len(my_process))
                 cpu_usage = m.cpu_percent();
             
                 rss = m.memory_info().rss;
@@ -62,8 +74,10 @@ def data_collector(process_name, write_to_file, sample_interval, net_interface, 
                 sent_rate=0;
                 rec_rate=0;
                 occurance=0;
+                
                 for bw in bw_lines:
                     if m.name()in bw:
+                        
                         splitted_line=bw.split('\t',3);
                         if((str(m.pid)+"/") in splitted_line[0]):
                             occurance=occurance+1;
@@ -74,14 +88,17 @@ def data_collector(process_name, write_to_file, sample_interval, net_interface, 
                     rec_rate=rec_rate/occurance;         
                           
                 to_send_str = " %s %s %s %s %s %s %s" % (rospy.get_time(), m.name(), m.pid, cpu_usage, rss, sent_rate, rec_rate)  
-                if(not sample_interval):
+                
+                if(not write_to_file):
                     pub.publish(to_send_str)
                 else:
                     log_file.write(to_send_str+"\n")
             
             
             rospy.sleep(sample_interval)
-        except:
+        except :
+            print sys.exc_info()
+            my_process=reinitialize_process_list(process_name)
             pass
         
            
